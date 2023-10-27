63c580ecda73bbe9d62926093b5d6d0763da1549
simon <simon.hobbs@electrooptical.net>
2023-10-26 20:48:50 -0400

    initial commit

--- kicad_dsn_io/cli.py
+++ kicad_dsn_io/cli.py
@@ -4,22 +4,23 @@ import click
 import pcbnew
 
 
+@click.option("--board", required=True, help="PCBNEW PCB File")
+@click.option("--i", help="DSN file to import to board file")
+@click.option("--o", help="Output file name")
 @click.command()
-@click.argument("--board", required=True, help="PCBNEW PCB File")
-@click.argument("-i", help="DSN file to import to board file")
-@click.argument("-o", help="Output file name")
 def main(board, i, o) -> None:
     """Console script for kicad_dsn_io."""
-    b = pcbnew.LoadBoard(board)
     if i is None:
         if o is None:
             raise UserWarning("Needs either an import or export argument")
-        pcbnew(board, o)
+        b = pcbnew.LoadBoard(board)
+        pcbnew.ExportSpecctraDSN(o)
 
     else:
+        b = pcbnew.LoadBoard(board)
         pcbnew.ImportSpecctraSES(i)
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    main()  # pragma: no cover
